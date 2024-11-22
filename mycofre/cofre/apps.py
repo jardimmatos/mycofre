b597451567ffe68c9b9daebe2e701a2e6b5db2b4
Agostinho <jr.jardimmatos@gmail.com>
2024-11-22 12:12:59 -0300

    clean code cofre

--- mycofre/cofre/apps.py
+++ mycofre/cofre/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CofreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cofre'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cofre"
